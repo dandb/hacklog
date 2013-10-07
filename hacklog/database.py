c7a18755c94848063b68a83c69c7d85be36e09d7
Neil Rhine <nrhine@dandb.com>
2013-10-07 14:22:46 -0700

    HACKLOG-2 File format and method creation

--- hacklog/database.py
+++ hacklog/database.py
@@ -2,50 +2,50 @@ import sqlite3
 from sqlalchemy import *
 
 db = create_engine('sqlite:///hacklog.db')
-
+metadata = BoundMetaData(db)
 db.echo = True
 
-metadata = BoundMetaData(db)
-
-days = Table('days', metadata, 
-	Column('date', DateTime),
-	Column('username', String),
-	Column('profile', PickleType),)
-
-hours = Table('hours', metadata,
-	Column('date', DateTime),
-	Column('username', String),
-	Column('profile', PickleType),)
-
-servers = Table('servers', metadata,
-	Column('date', DateTime),
-	Column('username', String),
-	Column('profile', PickleType),)
-
-ipAddresses = Table('ip_address', metadata,
+def create_tables():
+	days = Table('days', metadata, 
 		Column('date', DateTime),
 		Column('username', String),
 		Column('profile', PickleType),)
 
-currentStatus = Table('current_status', metadata,
+	hours = Table('hours', metadata,
+		Column('date', DateTime),
+		Column('username', String),
+		Column('profile', PickleType),)
+
+	servers = Table('servers', metadata,
+		Column('date', DateTime),
+		Column('username', String),
+		Column('profile', PickleType),)
+
+	ipAddresses = Table('ip_address', metadata,
+		Column('date', DateTime),
+		Column('username', String),
+		Column('profile', PickleType),)
+
+	currentStatus = Table('current_status', metadata,
 		Column('username', String),
 		Column('date', DateTime),
 		Column('score', Integer),)
 
-eventLog = Table('event_log', metadata, 
+	eventLog = Table('event_log', metadata, 
 		Column('date', DateTime),
 		Column('username', String),
 		Column('ipAddress', String),
 		Column('success',),
 		Column('server', String),)
 
-metadata.create_all()
+	metadata.create_all()
 
-dayMapper = mapper(Days, days)
-hourMapper = mapper(Hours, hours)
-serverMapper = mapper(Servers, servers)
-ipAddressesMapper = mapper(IpAddresses, ipAddresses)
-currentStatusMapper = mapper(CurrentStatus, currentStatus)
-eventLogMapper = mapper(EventLog, eventLog)
+def create_object_mappings():
+	dayMapper = mapper(Days, days)
+	hourMapper = mapper(Hours, hours)
+	serverMapper = mapper(Servers, servers)
+	ipAddressesMapper = mapper(IpAddresses, ipAddresses)
+	currentStatusMapper = mapper(CurrentStatus, currentStatus)
+	eventLogMapper = mapper(EventLog, eventLog)
 
 
