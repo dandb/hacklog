90be98b6bfb657f0a93f57ae990832cc16a31983
afdsouza <adsouza@dandb.com>
2013-10-07 20:59:22 -0700

    added parser script that gets data from the syslogs also added a testing file

--- tests/xlog.py
+++ tests/xlog.py
@@ -1,14 +1,16 @@
+#import libraries for parsing
 from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, Regex
 from time import strftime
- 
+from collections import defaultdict
+import sys
+import re
+
+#class to parse data
 class Parser(object):
   def __init__(self):
     ints = Word(nums)
  
-    # priority
-    priority = Suppress("<") + ints + Suppress(">")
- 
-    # timestamp
+    # timestamp of the form <ints>:<ints>:<ints>
     month = Word(string.uppercase, string.lowercase, exact=3)
     day   = ints
     hour  = Combine(ints + ":" + ints + ":" + ints)
@@ -18,37 +20,61 @@ class Parser(object):
     # hostname
     hostname = Word(alphas + nums + "_" + "-" + ".")
  
-    # appname
+    # appname of the form <Word>[<ints>]:
     appname = Word(alphas + "/" + "-" + "_" + ".") + Optional(Suppress("[") + ints + Suppress("]")) + Suppress(":")
  
-    # message
+    # message from the logs
     message = Regex(".*")
   
     # pattern build
-    self.__pattern = priority + timestamp + hostname + appname + message
+    self.__pattern = timestamp + hostname + appname + message
     
   def parse(self, line):
     parsed = self.__pattern.parseString(line)
- 
+
     payload              = {}
-    payload["priority"]  = parsed[0]
-    payload["timestamp"] = strftime("%Y-%m-%d %H:%M:%S")
-    payload["hostname"]  = parsed[4]
-    payload["appname"]   = parsed[5]
-    payload["pid"]       = parsed[6]
-    payload["message"]   = parsed[7]
+    payload["month"]     = parsed[0]
+    payload["day"]       = parsed[1]
+    payload["time"]      = parsed[2]
+    payload["hostname"]  = parsed[3]
+    payload["appname"]   = parsed[4]
+    payload["pid"]       = parsed[5]
+    payload["message"]   = parsed[6]
     
     return payload
- 
-""" --------------------------------- """
- 
+
+def getDataFromMessage(message):
+    if "authentication failure" in message:
+        #print "Failed"
+        username = message.split('=')[-1]
+        loginUsers[username] += 1
+        #print username
+    else:
+        print "Passed"
+        big_regex = re.compile('|'.join(map(re.escape, unwantedWords)))
+        message = big_regex.sub(" ", message)
+        #print message
+        #matches = re.findall('\w+', message)
+        #print matches
+
+
+
+#main function
+#creates a Parse object, reads the logs line by line
 def main():
   parser = Parser()
-  
-  with open('./logs.csv') as syslogFile:
+  global loginUsers
+  loginUsers = {}
+  loginUsers = defaultdict(int)
+
+  global unwantedWords
+  unwantedWords = ["Accepted", "publickey", "for", "from", "port"]
+
+  with open(sys.argv[1]) as syslogFile:
     for line in syslogFile:
       fields = parser.parse(line)
-      print "parsed:", fields
-  
+      print fields
+      getDataFromMessage(fields["message"])
+
 if __name__ == "__main__":
   main()
