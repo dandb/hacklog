a4c1443985a676c5ed558449ed397319f70678ad
afdsouza <adsouza@dandb.com>
2013-10-10 16:00:58 -0700

    changed the test and parse.py file

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 sys.path.append('../hacklog')
 from parse import Parser
-from entities import EventLog
+from entities import *
 import re
 
 parse = Parser()
@@ -14,10 +14,12 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(1, 1)
 
     def test_parse_line_success(self):
-        self.assertIsInstance(parse.parseLogLine("<14>sshd[3070]: Accepted publickey for kantselovich from 10.42.10.2 port 2005 ssh2\x00"), EventLog)
+        sysLogMessage = SyslogMsg("<14>sshd[3070]: Accepted publickey for kantselovich from 10.42.10.2 port 2005 ssh2\x00", "192.168.56.1")
+        self.assertIsInstance(parse.parseLogLine(sysLogMessage), EventLog)
 
     def test_parse_line_failure(self):
-        self.assertIsInstance(parse.parseLogLine("<14>sshd[3070]: pam_unix(sshd:auth): authentication failure; login= uid=0 euid=0 tty=ssh ruser= rhost=10.42.10.22 user=msacks"), EventLog)
+        sysLogMessage = SyslogMsg("<14>sshd[3070]: pam_unix(sshd:auth): authentication failure; login= uid=0 euid=0 tty=ssh ruser= rhost=10.42.10.22 user=msacks", "192.168.56.1")
+        self.assertIsInstance(parse.parseLogLine(sysLogMessage), EventLog)
 
 def main():
     unittest.main()
