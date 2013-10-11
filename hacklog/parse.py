3a93fb21d4fcc205410cadc82acc151e6b105249
afdsouza <adsouza@dandb.com>
2013-10-10 22:04:51 -0700

    TEST-TOOL-3 Adding test configuration for parsing

--- hacklog/parse.py
+++ hacklog/parse.py
@@ -1,14 +1,16 @@
 from entities import EventLog
 from entities import SyslogMsg
 from datetime import datetime
+import server
 import re
 
 class Parser():
-  def __init__(self, successPattern=None, failurePattern=None, successDateTimePatten=None, failureDateTimePattern=None):
+  def __init__(self, successPattern=None, failurePattern=None):
     self.successPattern = successPattern or 'Accepted\s+publickey\s+for\s+([0-9a-zA-Z_-]+)\s+from\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+port'
     self.failurePattern = failurePattern or 'pam_unix\(sshd:auth\):\s+authentication\s+failure\;\s+login=\s+uid=0\s+euid=0\s+tty=ssh+\s+ruser=+\s+rhost=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+user=([0-9a-zA-Z_-]+)'
-    self.successDateTimePatten = successDateTimePatten or 'Accepted\s+publickey\s+for\s+([0-9a-zA-Z_-]+)\s+from\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+port\s+(\d{1,4})+\s+ssh2+\s+DATE_TIME\s+(\d{1,4}-\d{1,2}-\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+HOST\s+([\w\+%\-& ]+)'
-    self.failureDateTimePattern = failureDateTimePattern or 'pam_unix\(sshd:auth\):\s+authentication\s+failure\;\s+login=\s+uid=0\s+euid=0\s+tty=ssh+\s+ruser=+\s+rhost=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+user=([0-9a-zA-Z_-]+)\s+DATE_TIME\s+(\d{1,4}-\d{1,2}-\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+HOST\s+([\w\+%\-& ]+)'
+    if server.testEnabled:
+        self.successPatten = 'Accepted\s+publickey\s+for\s+([0-9a-zA-Z_-]+)\s+from\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+port\s+(\d{1,4})+\s+ssh2+\s+DATE_TIME\s+(\d{1,4}-\d{1,2}-\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+HOST\s+([\w\+%\-& ]+)'
+        self.failurePattern = 'pam_unix\(sshd:auth\):\s+authentication\s+failure\;\s+login=\s+uid=0\s+euid=0\s+tty=ssh+\s+ruser=+\s+rhost=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+user=([0-9a-zA-Z_-]+)\s+DATE_TIME\s+(\d{1,4}-\d{1,2}-\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+HOST\s+([\w\+%\-& ]+)'
   def parseLogLine(self, message):
 
     returnEvent = False
@@ -24,7 +26,7 @@ class Parser():
                 logline.pop(0)
                 log_entry = ' '.join(logline)
                 # successful login
-                m = re.match(self.successDateTimePatten, log_entry)
+                m = re.match(self.successPatten, log_entry)
                 if m:
                     user_name = m.groups(0)[0]
                     user_ip = m.groups(0)[1]
@@ -34,7 +36,7 @@ class Parser():
                     returnEvent = EventLog(date_time, user_name, user_ip, True, host)
 
                 # login failed
-                m = re.match(self.failureDateTimePattern, log_entry)
+                m = re.match(self.failurePattern, log_entry)
                 if m:
                     user_name = m.groups(0)[1]
                     user_ip = m.groups(0)[0]
